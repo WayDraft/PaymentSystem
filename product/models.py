7a497e771966e4485ab117f35c955b95dd42c978
ClaireGYKim <clairekimgy@gmail.com>
2025-12-16 11:02:42 +0900

    Add product/models.py

--- product/models.py
+++ product/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True) 
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products')
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    stock_quantity = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    image_url = models.URLField(max_length=500, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
